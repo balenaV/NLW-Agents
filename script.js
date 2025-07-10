7c55652bd035063402491cf457d149158a24d533
Victor Balena <victorbalenaes@gmail.com>
2025-07-09 22:30:00 -0300

    Finalizando Front-End e Iniciando JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiKeyInput = document.getElementById("apiKey");
+const gameSelect = document.getElementById("gameSelect");
+const questionInput = document.getElementById("questionInput");
+const askButton = document.getElementById("askButton");
+const aiResponse = document.getElementById("aiResponse");
+const form = document.getElementById("form");
+
+const sendForm = (event) => {
+  event.preventDefault();
+  console.log(event);
+}; // Criando função de enviar formulario
+form.addEventListener("submit", sendForm);
